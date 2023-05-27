f75fcff5ab71d221a9c08d6c54d25f86f27770bd
PabloC2A <pabloandrescriolloalvarez@gmail.com>
2023-05-27 10:32:16 -0500

    Parte 1

--- AdministrarInmobiliaria/src/paquete01/Ejecutor.java
+++ AdministrarInmobiliaria/src/paquete01/Ejecutor.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete01;
 
-/**
- *
- * @author reroes
- * @author Pablo Andres Criollo Alvarez
- */
 public class Ejecutor {
-    
+    public static void main(String[] args) {
+
+    }
 }
